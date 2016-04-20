b7a71c9e9393f688baa634757e1a2c0a782e13de
Alex Harrison <alexkendall.harrison@gmail.com>
2016-04-20 09:53:13 -0400

    Added network mode, Nearby Dependency, and Skeleton for NM

--- SwiftyConnections/ViewController.swift
+++ SwiftyConnections/ViewController.swift
@@ -1,18 +1,11 @@
-//
-//  ViewController.swift
-//  SwiftyConnections
-//
-//  Created by Alexander Harrison on 4/20/16.
-//  Copyright © 2016 Alexander Harrison. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    var networkManager = NetworkManager(mode: .Client)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
